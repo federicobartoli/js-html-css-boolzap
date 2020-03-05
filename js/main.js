8c45b740a5de7d24f2cc03d008ee1f25cea3cb41
Federico <federicobartoli@me.com>
2020-03-05 11:33:51 +0100

    Aggiunte prime cose con javascript

--- js/main.js
+++ js/main.js
@@ -1,9 +1,39 @@
 $(document).ready(function(){
 
+     // $(document).keydown(function(event){}
+
+     $('#invia-messaggio').click(function(){
+          var messaggioUtente = $('.search-bar-input-messaggio').val();
+          console.log(messaggioUtente);
+          $('.search-bar-input-messaggio').val('');
+          var messaggio = $('.main-chat.sxs .chat-you').clone();
+          console.log(messaggio);
+          messaggio.children('p').text(messaggioUtente);
+          $('.main-chat.sxs2').append(messaggio);
+
+
+     });
+
+
+
+
+     $(".chat-object").click(chat);
+
+
+     // Funzione per selezionare la chat
+       function chat() {
+           // Tolgo la classe active a tutti ..
+           $(".chat-object").removeClass("active");
+           $(this).addClass("active");
+           var attributo = $(this).attr('nome-utente');
+           $("#nome-utente").text(attributo);
+           var immagine = $(this).attr('avatar');
+           $("#avatar").text(immagine);
+
+       }
 
 
 
 
 
-     
 })
